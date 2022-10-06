8a984ce3f3b2a20a8df193c884f6ce03247e93a0
Gopal S Akshintala <gopalakshintala@gmail.com>
2022-10-06 14:08:55 +0530

    :arrow_up: Upgrade Kover
    Signed-off-by: Gopal S Akshintala <gopalakshintala@gmail.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 val kotlinVersion: String by project
 dependencies {
-  implementation("org.jetbrains.kotlinx:kover:0.6.0")
+  implementation("org.jetbrains.kotlinx:kover:0.6.1")
   implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
   implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:5.0.10")
   implementation("io.freefair.lombok:io.freefair.lombok.gradle.plugin:6.5.0.3")
