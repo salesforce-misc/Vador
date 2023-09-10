af9494154d54189d0b9ba6d340ce87b5a3049bc8
Shaily <31181254+Shaily20@users.noreply.github.com>
2023-09-10 19:53:59 +0530

    Add MaxForInt and MinForInt annotations

---
+++ vador/src/main/java/com/salesforce/vador/annotation/MaxForInt.kt
@@ -0,0 +1,23 @@
+package com.salesforce.vador.annotation
+
+import com.salesforce.vador.types.ValidatorAnnotation2
+import java.lang.reflect.Field
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FIELD)
+annotation class MaxForInt(val limit: Int, val failureKey: String)
+
+class MaxForIntValidator<FailureT> : ValidatorAnnotation2<Int, FailureT> {
+  override fun validate(
+    field: Field,
+    value1: Int,
+    value2: Int,
+    failure: FailureT,
+    none: FailureT
+  ): FailureT {
+    if (value1 > value2) {
+      return failure
+    }
+    return none
+  }
+}
