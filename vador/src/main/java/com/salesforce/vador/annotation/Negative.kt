852874992cbc996fa16ced89d674a1314ebed5b5
Shaily <31181254+Shaily20@users.noreply.github.com>
2023-09-10 19:51:42 +0530

    Update Negative.kt

--- vador/src/main/java/com/salesforce/vador/annotation/Negative.kt
+++ vador/src/main/java/com/salesforce/vador/annotation/Negative.kt
@@ -3,7 +3,11 @@ package com.salesforce.vador.annotation
 import com.salesforce.vador.types.ValidatorAnnotation1
 import java.lang.reflect.Field
 
-class Negative<FailureT> : ValidatorAnnotation1<Int, FailureT> {
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.FIELD)
+annotation class Negative(val failureKey: String)
+
+class NegativeValidator<FailureT> : ValidatorAnnotation1<Int, FailureT> {
   override fun validate(field: Field, value: Int, failure: FailureT, none: FailureT): FailureT {
     if (value > -1) {
       return failure
